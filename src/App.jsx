b341873ad037e644ac61a070a434222fad3beb99
Lukáš Sitter <lukas.sitter@gmail.com>
2022-07-01 22:31:41 -0400

    Implement hooks

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="app-title">Monsters Roledex</h1>
+      <h1 className="app-title">Monsters Rolodex</h1>
       <SearchBox onChangeHandler={onSearchChange} placeholder="Search monsters" className="monsters-search-box" />
       <CardList monsters={filteredMonsters} />
     </div>
